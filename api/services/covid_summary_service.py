82ec05413611e6164a88aea9e5636ffaca3b791d
Gerardo Vitale <gerardo.vitale.errico@gmail.com>
2021-08-12 17:15:17 +0200

    paginate_mongo_data and calculate_rates implemented

--- api/services/covid_summary_service.py
+++ api/services/covid_summary_service.py
@@ -1,10 +1,10 @@
 import datetime
-from typing import Any, Dict
+from typing import Any, Dict, List
 
-from pymongo import MongoClient
+from pymongo import MongoClient, CursorType, DESCENDING
 
 
-def find_covid_summary(database: MongoClient, days: int, location: str) -> Dict[str, Any]:
+def find_covid_summary(database: MongoClient, days: int, location: str) -> CursorType:
     to_date = datetime.datetime.today()
     from_date = to_date - datetime.timedelta(days=days)
     query = {'$and': [{'date': {'$gte': from_date,
@@ -20,8 +20,17 @@ def find_covid_summary(database: MongoClient, days: int, location: str) -> Dict[
                'hosp_patients':1,
                'total_cases':1,
                'total_deaths':1}
-    result = list(database['covid_deaths'].find(query, project))
-    for row in result:
-        row['infected_rate'] = row['total_cases'] / row['population'] * 100
-        row['death_rate'] = row['total_deaths'] / row['total_cases'] * 100
-    return result
+    result = database['covid_deaths'].find(query, project).sort('date', DESCENDING)
+    return result, to_date, from_date
+
+
+def paginate_mongo_data(mongo_data: CursorType, start: int, width: int) -> CursorType:
+    return mongo_data.skip(start).limit(width)
+
+
+def calculate_rates(mongo_data: CursorType) -> List[Dict[str, Any]]:
+    mongo_data = list(mongo_data)
+    for doc in mongo_data:
+        doc['infected_rate'] = doc['total_cases'] / doc['population'] * 100
+        doc['death_rate'] = doc['total_deaths'] / doc['total_cases'] * 100
+    return mongo_data
