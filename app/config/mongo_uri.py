7922a9763cb923ac51ad0281b7a5a0d45fc4c07c
Gerardo Vitale <gerardo.vitale.errico@gmail.com>
2021-08-14 13:49:56 +0200

    config folder created to host mongo_uri and spark_session

---
+++ app/config/mongo_uri.py
@@ -0,0 +1,4 @@
+MONGODB_URI = "mongodb://mongo_db:27017/covid-project"
+MONGODB_URI_COVID_DEATHS = f"{MONGODB_URI}.covid_deaths"
+MONGODB_URI_COVID_VACCINATIONS = f"{MONGODB_URI}.covid_vaccinations"
+MONGODB_URI_NEW_CASES_CHART_DATA = f"{MONGODB_URI}.total_new_cases_chart_data"
---
+++ app/config/spark_session.py
@@ -0,0 +1,15 @@
+import findspark
+findspark.init('/usr/local/spark-3.1.2-bin-hadoop3.2')
+
+import pymongo
+import pymongo_spark
+pymongo_spark.activate()
+
+from pyspark.sql import SparkSession
+
+
+# PySpark connector model
+spark = SparkSession.builder.appName("CovidApp") \
+                .config("spark.jars.packages",
+                        "org.mongodb.spark:mongo-spark-connector_2.12:3.0.1",)\
+                .getOrCreate()
