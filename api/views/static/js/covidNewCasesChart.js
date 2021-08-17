ed1a307d6868fa0046bb4b3390911670498d69d8
Gerardo Vitale <gerardo.vitale.errico@gmail.com>
2021-08-17 21:15:21 +0200

    js to plot covid_total_new_cases created

---
+++ api/views/static/js/covidNewCasesChart.js
@@ -0,0 +1,47 @@
+const country = document.getElementById('country').value
+
+fetch('http://localhost:5000/covid_new_cases.json/' + country)
+    .then(response => response.json())
+    .then(mongoData => printCharts(mongoData))
+
+
+function printCharts(mongoData) {
+    document.body.classList.add('running')
+    barChart(mongoData.result, 'chart')
+}
+
+
+function getUnique(value, index, self) {
+    return self.indexOf(value) === index
+  }
+
+
+function barChart(mongoData, id) {
+    const yearsInMongoData = mongoData.map(eachRecord => eachRecord.year)
+    const records = yearsInMongoData.filter(getUnique).map((year, index) => {
+        return {
+            label: year,
+            data: mongoData.filter(eachRecord => eachRecord.year === year)
+                           .map(eachRecord => eachRecord.total_new_cases),
+            borderWidth: 1,
+            borderColor: styles.color.solids[index],
+            backgroundColor: styles.color.alphas[index]
+        }
+    })
+
+    const data = {
+        labels: ['jan', 'feb', 'mar', 'apr', 'may', 'jun', 'jul', 'aug', 'sep', 'oct', 'nov', 'dec'],
+        datasets : records
+    }
+
+    const options = {
+        legend: {
+            position: 'right',
+            labels: {
+                fontColor: 'grey'
+            }
+        }
+    }
+    
+    new Chart(id, { type: 'bar', data, options })
+}
