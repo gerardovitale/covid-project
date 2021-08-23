52f575e3bd57227fe8e96c327b514f53e14158fc
Gerardo Vitale <gerardo.vitale.errico@gmail.com>
2021-08-23 18:55:47 +0200

    decorator created to time functions and methods

--- app/app.py
+++ app/app.py
@@ -1,7 +1,8 @@
 from pipelines.manage_covid_database import (get_covid_data,
                                              save_covid_data)
 from pipelines.publish_data_to_mongo import (publish_covid_dataset,
-                                             publish_total_new_cases_chart_data)
+                                             publish_total_new_cases_chart_data,
+                                             publish_total_new_deaths_chart_data)
 
 
 if __name__ == "__main__":
@@ -11,3 +12,4 @@ if __name__ == "__main__":
     publish_covid_dataset()
 
     publish_total_new_cases_chart_data()
+    publish_total_new_deaths_chart_data()
