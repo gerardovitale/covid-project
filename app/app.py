d54c6fd6ccaf0e1e39298bfcbb51c6af33adf622
Gerardo Vitale <gerardo.vitale.errico@gmail.com>
2021-10-12 10:49:03 +0200

    project refactored

--- app/app.py
+++ app/app.py
@@ -1,17 +1,16 @@
-from pipelines.manage_covid_database import (get_covid_dataset,
-                                             save_covid_data,
-                                             correct_january_records_mongodb)
-from pipelines.publish_data_to_mongo import (publish_covid_dataset,
-                                             publish_total_new_cases_chart_data,
-                                             publish_total_new_deaths_chart_data)
-
+from pipelines.extract.extract_covid_dataset import get_covid_dataset
+from pipelines.load.save_data_locally import save_covid_data_locally
+from pipelines.transform.transform_covid_dataset import (add_missing_january_records_mongodb, manage_null_values)
+from pipelines.load.publish_data_to_mongo import (publish_covid_data_to_mongo,
+                                                  publish_total_new_cases_chart_data,
+                                                  publish_total_new_deaths_chart_data)
 
 if __name__ == "__main__":
-    dataset = get_covid_dataset()
-    save_covid_data(dataset)
-
-    publish_covid_dataset()
-    correct_january_records_mongodb()
+    covid_data = get_covid_dataset()
+    covid_data = manage_null_values(covid_data)
+    save_covid_data_locally(covid_data)
+    publish_covid_data_to_mongo()
+    add_missing_january_records_mongodb()
 
     publish_total_new_cases_chart_data()
     publish_total_new_deaths_chart_data()
