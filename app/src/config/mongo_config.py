2084da8f3dd553415e0717ed5dffb4da6f5883da
Gerardo Vitale <gerardo.vitale.errico@gmail.com>
2021-10-12 10:49:58 +0200

    minor change

--- app/src/config/mongo_config.py
+++ app/src/config/mongo_config.py
@@ -5,4 +5,4 @@ MONGODB_URI = f"{MONGODB_HOST}/covid-project"
 MONGODB_URI_COVID = f"{MONGODB_URI}.covid_dataset"
 MONGODB_URI_NEW_CASES_CHART_DATA = f"{MONGODB_URI}.total_new_cases_chart_data"
 
-client = MongoClient(f"{MONGODB_HOST}")
+mongo_client = MongoClient(f"{MONGODB_HOST}")
