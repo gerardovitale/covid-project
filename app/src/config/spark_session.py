22a39e7b26c8ad3a6b6ddd41c16c47035f4ebef0
Gerardo Vitale <gerardo.vitale.errico@gmail.com>
2021-12-05 01:09:32 +0100

    .master added

--- app/src/config/spark_session.py
+++ app/src/config/spark_session.py
@@ -6,6 +6,7 @@ from pyspark.sql import SparkSession
 
 # PySpark connector model
 spark = SparkSession.builder.appName("CovidApp") \
+                    .master("local[*]") \
                     .config("spark.jars.packages",
                             "org.mongodb.spark:mongo-spark-connector_2.12:3.0.1",)\
                     .getOrCreate()
