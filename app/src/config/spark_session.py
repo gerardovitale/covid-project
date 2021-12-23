d91548e0c16c7d6aa696beeb25c2b83eaef8d5ed
Gerardo Vitale <gerardo.vitale.errico@gmail.com>
2021-12-23 14:45:32 +0100

    SparkConf added

--- app/src/config/spark_session.py
+++ app/src/config/spark_session.py
@@ -1,12 +1,19 @@
 import findspark
+from pyspark import SparkConf
+
 findspark.init('/usr/local/spark-3.1.2-bin-hadoop3.2')
 
 from pyspark.sql import SparkSession
 
+conf = SparkConf().setAll([
+    ('spark.app.name', 'SparkApp'),
+    ('spark.executor.instances', 2),
+    ('spark.executor.cores', '4'),
+    ('spark.jars.packages', 'org.mongodb.spark:mongo-spark-connector_2.12:3.0.1')
+])
 
 # PySpark connector model
 spark = SparkSession.builder.appName("CovidApp") \
-                    .master("local[*]") \
-                    .config("spark.jars.packages",
-                            "org.mongodb.spark:mongo-spark-connector_2.12:3.0.1",)\
-                    .getOrCreate()
+    .master('local[*]') \
+    .config(conf=conf) \
+    .getOrCreate()
